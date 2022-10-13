4795142e76a1d5effad246258387beca16e6353c
JhuN <ceyhun_yildiz@hotmail.com>
2022-10-13 11:13:49 +0300

    2. class eklendi ayıp ayıp

--- src/Gun01/Jhun2.java
+++ src/Gun01/Jhun2.java
@@ -3,5 +3,6 @@ package Gun01;
 public class Jhun2 {
     public static void main(String[] args) {
         System.out.println("denee");
+        System.out.println("beni nasıl reddedersin");
     }
 }
